ed76d942b10114a659b9485ec25c2543bac872db
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-05-07 00:48:59 +0100

    finish

--- src/ProgressComponent.jsx
+++ src/ProgressComponent.jsx
@@ -40,17 +40,26 @@ function ProgressComponent({initialItems}) {
  const progressPercentage = Math.round((completedItems.size / items.length) * 100)
  const tasksLeft = items.length - completedItems.size;
 
+
+
   return (
     <div className='Progress-component'>
    <div className="checklist">
+   <h2>Checklist</h2>
     {items.map((item) => (
      <div key={item} className='checklist-item'>
     <input type="checkbox"
            checked={completedItems.has(item)}
            onChange={() => handleCheck(item)}
+
  />
  <label>{item}</label>
- <button className='delete-btn' onClick={() => handleDelete(item)}><BiTrash/></button>
+ <button className ='delete-btn' style={{
+  position: "relative",
+  float: "right",
+  background: "none",
+  border: "none"}} onClick={() => handleDelete(item)}><BiTrash style={{fontSize: "18px",
+  color: "orange"}}/></button>
    </div>
     ))}
     <div className="add-item">
@@ -60,15 +69,26 @@ function ProgressComponent({initialItems}) {
          value={newItem}
          onChange={(e) => setNewItem(e.target.value)}
          onKeyPress={(e) => e.key === 'Enter' && handleAdd()}
-         />
+         className='add-input'/>
          <button onClick={handleAdd}>Add</button>
     </div>
+
+    </div>
+    <div className="title-progressbar">
+    <div className="title">
+     <h2>Build your page</h2>
+     <p>Complete actions to unlock new features along the way.
+On average, completed pages get 30% more traffic.</p>
     </div>
     <div className="progress-bar">
      <div className="progress-bar-fill" style={{width: `${progressPercentage}%`}}/>
      <div className="progress-bar-percentage">{progressPercentage}%</div>
     </div>
-    <div className="tasks-left">Tasks left: {tasksLeft}</div>
+    <div className="tasks-left" style={{
+     marginTop: "20px", fontSize: "15px"
+
+    }}>Tasks left: <span>{tasksLeft}</span></div>
+    </div>
     </div>
   )
 }
